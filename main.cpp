fa8d01b9ff496ce9b1ae1c1bb42d74e2533fd81b
Lazzzycatwastaken <singleslayer69xxprogamer69xx@gmail.com>
2025-07-18 00:33:30 +0200

    Rewrite

--- main.cpp
+++ main.cpp
@@ -17,7 +17,26 @@ int main() {
         std::cin >> choice;
         std::cin.ignore();
 
-        switch (choice) {
+        switch (choice) {r/include/c++/15/cstdlib:46:28: error: ‘#include’ nested depth 200 exceeds maximum of 200 (use ‘-fmax-include-depth=DEPTH’ to increase the maximum)
+   46 | #include <bits/c++config.h>
+      |                            ^
+In file included from include/World.hpp:5:
+/usr/include/c++/15/ctime:46:28: error: ‘#include’ nested depth 200 exceeds maximum of 200 (use ‘-fmax-include-depth=DEPTH’ to increase the maximum)
+   46 | #include <bits/c++config.h>
+      |                            ^
+/usr/include/c++/15/ctime:47:18: error: ‘#include’ nested depth 200 exceeds maximum of 200 (use ‘-fmax-include-depth=DEPTH’ to increase the maximum)
+   47 | #include <time.h>
+      |                  ^
+src/Game.cpp:2:10: fatal error: SaveSystem.hpp: No such file or directory
+    2 | #include "SaveSystem.hpp"
+      |          ^~~~~~~~~~~~~~~~
+compilation terminated.
+
+ ✘  Thu 17 Jul - 23:56  ~/Desktop/VoidRoot   origin ☊ main ✔ 
+ @psi  
+
+ ✘  Thu 17 Jul - 23:56  ~/Desktop/VoidRoot   origin ☊ main ✔ 
+ @psi  
             case 1: {
                 Game game;
                 game.startNewGame();
